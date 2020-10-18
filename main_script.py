b688dbe00d20e1582162b9e690dfb1f9ad2b1dfe
FallenNephalem <ldr200@mail.ru>
2020-10-18 17:04:29 +0300

    analyse wifi script

--- main_script.py
+++ main_script.py
@@ -1,11 +1,7 @@
 from wifi import Cell, Scheme
-print(Cell.all('wlo1'))
 
 c = list(Cell.all('wlo1'))[0]
 print("SSID: {}".format(c.ssid))
 print("Mac: {}".format(c.address))
-print("signal: {}".format(c.signal))
-print("quality: {}".format(c.quality))
+print("RSSI: {}".format(c.signal))
 # https://www.youtube.com/watch?v=dQw4w9WgXcQ
-# https://issue.life/questions/53246654
-# https://xakep.ru/2018/11/09/wifi-hack/
