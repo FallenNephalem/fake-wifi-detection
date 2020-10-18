a37f268aafdcd6473ae2a41cef7ebf7ec4e3d6b0
FallenNephalem <ldr200@mail.ru>
2020-10-18 16:52:38 +0300

    analyse wifi script

--- main_script.py
+++ main_script.py
@@ -6,6 +6,6 @@ print("SSID: {}".format(c.ssid))
 print("Mac: {}".format(c.address))
 print("signal: {}".format(c.signal))
 print("quality: {}".format(c.quality))
-# https://www.youtube.com/watch?v=b_os0Gxs4e8
+# https://www.youtube.com/watch?v=dQw4w9WgXcQ
 # https://issue.life/questions/53246654
 # https://xakep.ru/2018/11/09/wifi-hack/
