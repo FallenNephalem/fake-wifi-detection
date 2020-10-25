9a19d40bc9caa662d5a39067627a123683c8e791
FallenNephalem <ldr200@mail.ru>
2020-10-25 15:41:52 +0300

    check parameters every 10 seconds and logging them

--- main_script.py
+++ main_script.py
@@ -1,7 +1,26 @@
-from wifi import Cell, Scheme
-
-c = list(Cell.all('wlo1'))[0]
-print("SSID: {}".format(c.ssid))
-print("Mac: {}".format(c.address))
-print("RSSI: {}".format(c.signal))
-# https://www.youtube.com/watch?v=dQw4w9WgXcQ
+from datetime import datetime
+from wifi import Cell
+import random, time, json
+while True:
+    c = list(Cell.all('wlo1'))[0]
+    params = open('params.json', 'r')
+    if params.read() == '':
+        params = open('params.json', 'w')
+        params.write(json.dumps({'SSID': c.ssid, 'Mac': c.address, 'RSSI': c.signal}))
+        params.close()
+        log = open('log.txt', 'a')
+        log.write('SSID: {0}, Mac: {1}, RSSI: {2} at {3} \n'.format(c.ssid, c.address, c.signal, datetime.now().time()))
+        log.close()
+    else:
+        params = open('params.json', 'r')
+        params_dict = json.loads(params.read())
+        if (params_dict['SSID'] != c.ssid) | (params_dict['Mac'] != c.address) | (params_dict['RSSI'] != c.signal):
+            log = open('log.txt', 'a')
+            log.write('SSID: {0}, Mac: {1}, RSSI: {2} at {3} \n'.format(c.ssid, c.address, c.signal, datetime.now().time()))
+            params.close()
+            log.close()
+            params = open('params.json', 'w')
+            params.write(json.dumps({'SSID': c.ssid, 'Mac': c.address, 'RSSI': c.signal}))
+            params.close()
+            print('SSID: {0}, Mac: {1}, RSSI: {2} at {3} \n'.format(c.ssid, c.address, c.signal, datetime.now().time()))
+            time.sleep(10)
